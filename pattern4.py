688827571038f36cbb8c14a41c8cbf594e05b320
sumitgondaliya <32106486+sumitgondaliya@users.noreply.github.com>
2020-12-10 20:16:50 +0530

    Create pattern4.py

---
+++ pattern4.py
@@ -0,0 +1,21 @@
+""" 
+pattern: 
+        *
+        * *
+        * * *
+        * *
+        * 
+Make Sure: No. of rows should be an odd number.
+"""
+n = int(input("Enter No. of Rows:"))
+m = (n//2)+1        
+for i in range(1,n+1):
+    if i <= m:
+        j = 1
+        while j <= i:
+            print("*", end=" ")
+            j += 1
+    else:
+        for k in range(m+1,(n+m-(i-1))+1):
+            print("*",end=" ")
+    print()
